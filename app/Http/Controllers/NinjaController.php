57d95e66a17df8c2b08c6972842871bc56c64537
Mykola <mikolatovarnickij74@gmail.com>
2025-11-01 19:03:07 +0200

    More on controllers

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -13,10 +13,13 @@ class NinjaController extends Controller
     }
     
     public function show($id) {
-
+        $ninja = Ninja::findOrFail($id);
+        
+        return view('ninjas.show', ["ninja" => $ninja]);
     }
 
     public function create() {
+        return view('ninjas.create');
 
     }
 }
--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <h2>Ninja id - {{$id}}</h2>
+    <h2>{{$ninja->name}}</h2>
+    <div class="bg-gray-200 p-4 rounded"></div>
+    <p><strong>Skill level: </strong>{{ $ninja->skill }}</p>
+    <p>About me: {{ $ninja->bio }}</p>
 </x-layout>
